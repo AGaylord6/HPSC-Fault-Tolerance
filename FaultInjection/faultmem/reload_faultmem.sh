6546a7dd8b061808f7c28f1855be5347329acb22
Andrew Gaylord <113562174+AGaylord6@users.noreply.github.com>
2026-05-05 14:25:22 -0400

    Update reload script

--- FaultInjection/faultmem/reload_faultmem.sh
+++ FaultInjection/faultmem/reload_faultmem.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-sudo rmmod faultmem.ko
+set -e
+
+# Remove module only if it's currently loaded
+if lsmod | grep -q '^faultmem\b'; then
+	echo "faultmem is loaded, removing"
+	sudo rmmod faultmem
+fi
+
+echo "Building faultmem module"
 make
+
+echo "Inserting module"
 sudo insmod faultmem.ko
-echo "\n"
-sudo lsmod | grep faultmem
+
+echo
+sudo lsmod | grep faultmem || echo "faultmem not present in lsmod."
